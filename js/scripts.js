0298954fc3e80073a010f4a526f4834c227cd6ec
vladliena <sotnikovavladlena@gmail.com>
2022-12-20 12:38:04 +0200

    deleted second loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 function unique (arrowOne, arrowTwo){
-    let arrowMain = []
-    for (let i = 0; i < arrowOne.length; i++){
-        let arrowOneElement = arrowOne[i]
-        if (!(arrowTwo.includes(arrowOneElement))){
-            arrowMain.push(arrowOneElement)
-        }
-        for (let j = 0; j < arrowTwo.length; j++){
-            let arrowTwoElement = arrowTwo[i]
-            if (!(arrowOne.includes(arrowTwoElement))){
-                arrowMain.push(arrowTwoElement)
-                break
-            }
+    const arrowMain = []
+    for (let i = 0; i < arrowTwo.length; i++){
+        let arrowTwoElement = arrowTwo[i]
+        if (!(arrowOne.includes(arrowTwoElement))) {
+            arrowMain.push(arrowTwoElement);
+        } else if (!(arrowMain.includes(arrowOne[i]))) {
+            arrowMain.push(arrowOne[i])
         }
     }
     return arrowMain
