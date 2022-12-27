ae4f370f7de6da08c322058a3e444abac08be243
vladliena <sotnikovavladlena@gmail.com>
2022-12-27 17:14:46 +0200

    last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-function unique (arrowOne, arrowTwo){
+// function unique (arrowOne, arrowTwo){
+//     const arrowMain = []
+//     for (let i = 0; i < arrowTwo.length; i++){
+//         let arrowTwoElement = arrowTwo[i]
+//         if (!(arrowOne.includes(arrowTwoElement))) {
+//             arrowMain.push(arrowTwoElement);
+//         } else if (!(arrowMain.includes(arrowOne[i]))) {
+//             arrowMain.push(arrowOne[i])
+//         }
+//     }
+//     return arrowMain
+// }
+//
+// console.log(unique(['Dnipro', 'Paris', 'York', 'Ternopil'],['Lviv', 'Dnipro', 'Ternopil', 'London']))
+
+
+
+function unique (arrowOne, arrowTwo) {
     const arrowMain = []
-    for (let i = 0; i < arrowTwo.length; i++){
-        let arrowTwoElement = arrowTwo[i]
-        if (!(arrowOne.includes(arrowTwoElement))) {
-            arrowMain.push(arrowTwoElement);
-        } else if (!(arrowMain.includes(arrowOne[i]))) {
+    for (let i = 0; i < arrowTwo.length; i++) {
+        if (arrowOne.indexOf(arrowTwo[i]) == -1){
+            arrowMain.push(arrowTwo[i])
+        } else {
             arrowMain.push(arrowOne[i])
         }
     }
